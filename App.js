6a56cfd0c3fabc1aafbd36927a26ecb78b41d241
Luiz Rioja <lulyrioja@gmail.com>
2021-12-23 23:35:14 -0300

    :alien: Finishing the carousel's parallax effect

--- App.js
+++ App.js
@@ -15,14 +15,14 @@ const ITEM_WIDTH = width * 0.76;
 const ITEM_HEIGHT = ITEM_WIDTH * 1.47;
 
 const images = [
-  "https://images.unsplash.com/photo-1551316679-9c6ae9dec224?w=800&q=80",
   "https://images.unsplash.com/photo-1562569633-622303bafef5?w=800&q=80",
+  "https://images.unsplash.com/photo-1517957754642-2870518e16f8?w=800&q=80",
+  "https://images.unsplash.com/photo-1511208687438-2c5a5abb810c?w=800&q=80",
+  "https://images.unsplash.com/photo-1546484959-f9a381d1330d?w=800&q=80",
+  "https://images.unsplash.com/photo-1551316679-9c6ae9dec224?w=800&q=80",
   "https://images.unsplash.com/photo-1503656142023-618e7d1f435a?w=800&q=80",
   "https://images.unsplash.com/photo-1555096462-c1c5eb4e4d64?w=800&q=80",
-  "https://images.unsplash.com/photo-1517957754642-2870518e16f8?w=800&q=80",
-  "https://images.unsplash.com/photo-1546484959-f9a381d1330d?w=800&q=80",
   "https://images.unsplash.com/photo-1548761208-b7896a6ff225?w=800&q=80",
-  "https://images.unsplash.com/photo-1511208687438-2c5a5abb810c?w=800&q=80",
   "https://images.unsplash.com/photo-1548614606-52b4451f994b?w=800&q=80",
   "https://images.unsplash.com/photo-1548600916-dc8492f8e845?w=800&q=80",
 ];
@@ -35,23 +35,91 @@ const data = images.map((image, index) => ({
 }));
 
 export default function App() {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      <Text style={{ fontSize: 114 }}>❤️</Text>
-      <Text
-        style={{
-          fontFamily: "Roboto",
-          marginTop: 10,
-          fontWeight: "800",
-          fontSize: 32,
+
+      <Animated.FlatList
+        data={data}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: true }
+        )}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item, index }) => {
+          const inputRange = [
+            (index - 1) * width,
+            index * width,
+            (index + 1) * width,
+          ];
+
+          const translateX = scrollX.interpolate({
+            inputRange,
+            outputRange: [-width * 0.7, 0, width * 0.7],
+          });
+
+          return (
+            <View
+              style={{ width, justifyContent: "center", alignItems: "center" }}
+            >
+              <View
+                style={{
+                  borderRadius: 18,
+                  shadowColor: "#000",
+                  shadowOpacity: 0.5,
+                  shadowRadius: 30,
+                  shadowOffset: {
+                    width: 0,
+                    height: 0,
+                  },
+                  padding: 12,
+                  elevation: 25,
+                  backgroundColor: "white",
+                }}
+              >
+                <View
+                  style={{
+                    width: ITEM_WIDTH,
+                    height: ITEM_HEIGHT,
+                    overflow: "hidden",
+                    alignItems: "center",
+                    borderRadius: 14,
+                  }}
+                >
+                  <Animated.Image
+                    source={{ uri: item.photo }}
+                    style={{
+                      width: ITEM_WIDTH * 1.4,
+                      height: ITEM_HEIGHT,
+                      resizeMode: "cover",
+                      transform: [{ translateX }],
+                    }}
+                  />
+                </View>
+
+                <Image
+                  source={{ uri: item.avatar_url }}
+                  style={{
+                    width: 60,
+                    height: 60,
+                    borderRadius: 60,
+                    borderWidth: 6,
+                    borderColor: "white",
+                    position: "absolute",
+                    bottom: -30,
+                    right: 60,
+                  }}
+                />
+              </View>
+            </View>
+          );
         }}
-      >
-        Expo
-      </Text>
-      <Text style={{ fontFamily: "Roboto", fontStyle: "italic", fontSize: 18 }}>
-        (expo.io)
-      </Text>
+      />
     </View>
   );
 }
